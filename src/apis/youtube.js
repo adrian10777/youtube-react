a87de9bde3be46cf271e53fe81e6fc0c3be97776
Adrian Henriquez <adrian10777@hotmail.com>
2022-02-07 18:44:53 -0500

    firstcommit

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyAzg3CV7mLMiykShPUM5bw2F8_nN47N3ms;'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 10,
+        key: KEY
+
+    }
+});
