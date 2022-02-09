194faa3b31004b83f21abbac54d2feee811d2416
Adrian Henriquez <adrian10777@hotmail.com>
2022-02-09 16:58:09 -0500

    secondcommit

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,13 +1,19 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
-    const renderedList = videos.map((video) => {
-        return <VideoItem video={video} />
+const VideoList = ({ videos, onVideoSelect }) => {
+    const renderedList = videos.map(video => {
+        console.log(renderedList)
+        return (
+            <VideoItem
+                key={video.id.videoId}
+                onVideoSelect={onVideoSelect}
+                video={video}
+            />
+        );
     });
 
-    return 
-    <div className="ui relaxed divided list">{renderedList}</div>
+    return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
